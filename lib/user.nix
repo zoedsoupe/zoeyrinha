5f3be35f10ee5726fc274a384521f515c196e8f9
Zoey Pessanha <zoey.spessanha@outlook.com>
2022-04-20 03:24:27 -0300

    feat: create user config function :sparkles:

--- lib/user.nix
+++ lib/user.nix
@@ -3,7 +3,43 @@
 with builtins;
 
 {
-  mkHMUser = {};
+  mkHMUser = { userConfig, username }:
+    home-manager.lib.homeManagerConfiguration {
+      inherit system username pkgs;
+      stateVersion = "21.05";
+      configuration =
+        let
+          trySettings = tryEval (fromJSON (readFile /etc/hmsystemdata.json));
+          machineData = if trySettings.success then trySettings.value else {};
+
+          machineModule = { pkgs, config, lib, ... }: {
+            options.machineData = lib.mkOption {
+              default = {};
+              description = "Settings passed from nixos system configuration. If not present will be empty";
+            };
+
+            config = { inherit machineData };
+          };
+        in {
+          zoedsoupe = userConfig;
+
+          nixpkgs = {
+            inherit overlays;
+            config.allowUnfree = true;
+          };
+
+          systemd.user.startServices = true;
+
+          home = {
+            inherit username;
+            stateVersion = "21.05";
+            homeDirectory = "/home/${username}";
+          };
+
+          imports = [ ../modules/users machineModule ];
+        };
+      homeDirectory = "/home/${username}";
+    };
 
   mkSystemUser = { name, groups, uid, shell }: {
     users.users."${name}" = {
