d134bf62b4035810592cc9f894ee112450263dc0
Zoey Pessanha <zoey.spessanha@outlook.com>
2022-04-20 04:10:35 -0300

    feat: direnv and fzf modules :sparkles:

---
+++ modules/users/direnv.nix
@@ -0,0 +1,25 @@
+{ pkgs, config, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.zoedsoupe.direnv;
+in {
+  options.zoedsoupe.direnv = {
+    enable = mkOption {
+      description = "Enable direnv";
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf (cfg.enable) {
+    programs.direnv = {
+      enable = true;
+      nix-direnv = {
+        enable = true;
+        enableFlakes = true;
+      };
+    };
+  };
+}
