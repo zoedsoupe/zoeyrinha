5520c574d2bbe25503f2197f7c17334afa3379a5
Zoey Pessanha <zoey.spessanha@outlook.com>
2022-04-20 05:31:27 -0300

    feat: add sway module :sparkles:

---
+++ modules/system/graphical/xorg.nix
@@ -0,0 +1,41 @@
+{ pkgs, config, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.zoedsoupe.graphical.xorg;
+in
+{
+  options.zoedsoupe.graphical.xorg = {
+    enable = mkOption {
+      description = "Enable xserver";
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf (cfg.enable) {
+    services.xserver = {
+      enable = true;
+      layout = "us";
+      xkbOptions = "ctrl:swapcaps";
+      xkbVariant = "intl";
+      libinput = {
+        enable = true;
+        touchpad = {
+          tapping = true;
+          disableWhileTyping = true;
+          naturalScrolling = true;
+        };
+      };
+      updateDbusEnvironment = true;
+      serverFlagsSection = ''
+        Option "BlankTime" "180"
+        Option "StandbyTime" "45"
+        Option "SuspendTime" "45"
+        Option "OffTime" "180"
+      '';
+      displayManager.startx.enable = true;
+    };
+  };
+}
---
+++ modules/users/graphical/config.nix
@@ -0,0 +1,28 @@
+{ pkgs, config, lib, ... }:
+
+with lib;
+
+{
+  config.zoedsoupe.graphical = {
+    wayland = {
+      enable = mkDefault false;
+      type = mkDefault null;
+
+      background = {
+        enable = mkDefault false;
+        image = mkDefault ./wallpapers/.jpg;
+        mode = mkDefault "fill";
+        pkg = mkDefault pkgs.swaybg;
+      };
+
+      statusbar = {
+        enable = mkDefault false;
+        pkg = mkDefault pkgs.waybar;
+      };
+
+      screenlock = {
+        enable = mkDefault false;
+      };
+    };
+  };
+}
