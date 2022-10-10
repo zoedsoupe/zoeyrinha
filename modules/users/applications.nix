b33692fe85fbe798ee37727959ffacfdbdb7af71
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-10-10 13:53:51 -0300

    upgrade and need to install solargraph as global :arrow_up: :sparkles:

--- modules/users/applications.nix
+++ modules/users/applications.nix
@@ -16,48 +16,6 @@ in
   };
 
   config = mkIf (cfg.enable) {
-    xdg.configFile."nvim/coc-settings.json".source = pkgs.writeTextFile {
-      name = "coc-settings.json";
-      text = builtins.toJSON {
-        "codeLens.enable" = true;
-        "rust-client" = {
-          "disableRustup" = true;
-          "rlsPath" = "${pkgs.rls}/bin/rls";
-        };
-        "suggest" = {
-          "noselect" = true;
-          "removeDuplicateItems" = true;
-        };
-        "languageserver" = {
-          "elixirLS" = {
-            "command" = "${pkgs.elixir_ls}/bin/elixir-ls";
-            "filetypes" = [ "elixir" "eexlixir" "heexlixir" ];
-          };
-          "rust" = {
-            "command" = "${pkgs.rls}/bin/rls";
-            "rootPatterns" = [ "Cargo.toml" ];
-            "filetypes" = [ "rs" ];
-          };
-          "nix" = { "command" = "${pkgs.rnix-lsp}/bin/rnix-lsp"; "filetypes" = [ "nix" ]; };
-          # FIX ME
-          # "solargraph" = {
-          #   "commandPath" = "${pkgs.solargraph}/bin";
-          #   "filetypes" = [ "rb" "erb" ];
-          # };
-        };
-        "coc.preferences" = {
-          "useQuickfixForLocations" = true;
-          "snippets.enable" = true;
-          "extensionsUpdateCheck" = "never";
-          "formatOnSaveFiletypes" = [
-            "elixir"
-            "rust"
-            "nix"
-          ];
-        };
-      };
-    };
-
     programs = {
       home-manager.enable = true;
       command-not-found.enable = true;
@@ -77,7 +35,7 @@ in
 
       # agda
       # deno
-      solargraph # remove me
+      solargraph # required by coc-nvim (see copper)
       doctl
       google-cloud-sdk
       beekeeper-studio
