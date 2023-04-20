e73aaf2793750c7ffade205b41a9ca083cc3ceed
Zoey de Souza Pessanha <zoey.pessanha@outlook.com>
2023-04-20 15:35:01 -0300

    feat: move host specific config to hosts folder

--- hosts/mac/home.nix
+++ hosts/mac/home.nix
@@ -1,8 +1,45 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   home.username = "zoey.pessanha";
   home.homeDirectory = "/Users/zoey.pessanha";
 
   home.stateVersion = "22.11";
+
+  programs = {
+    home-manager.enable = true;
+    command-not-found.enable = false;
+  };
+
+  home.packages = with pkgs; [
+    # dev
+    copper # my neovim config
+
+    # agda
+    deno
+    nodejs-16_x
+    clojure
+    leiningen
+    stack
+    elixir_1_14
+    rustc
+    cargo
+    gitAndTools.gh
+    earthly
+    ngrok
+    nixpkgs-fmt
+    tmate
+
+    # tools
+    exercism
+    exa
+    ripgrep
+    fd
+
+    # others
+    any-nix-shell
+
+    # audio & video
+    ffmpeg
+  ];
 }
