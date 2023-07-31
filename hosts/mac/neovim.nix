45db10b2c84e14fea11e5f7be4f77fb9dbe0ef66
Zoey de Souza Pessanha <zoey.spessanha@gmail.com>
2023-07-31 15:41:10 -0300

    add different configs for neovim for different hosts

---
+++ hosts/mac/neovim.nix
@@ -0,0 +1,104 @@
+{mkNeovim}: let
+  config = {
+    lvim = {
+      autopair.enable = true;
+      comments.enable = true;
+      completion = {
+        enable = true;
+        buffer.enable = true;
+        cmdline.enable = false;
+        lsp = {
+          enable = true;
+          lspkind.enable = true;
+        };
+        path.enable = true;
+        snippets = {
+          enable = true;
+          source = "luasnip";
+        };
+      };
+      filetree = {
+        enable = false;
+        vinegar-style.enable = true;
+      };
+      git = {
+        enable = true;
+        gitsigns.enable = true;
+      };
+      lsp = {
+        enable = true;
+        clojure.enable = false;
+        css.enable = true;
+        dart.enable = false;
+        elixir.enable = true;
+        nix = {
+          enable = true;
+          nil.enable = true;
+          nixd.enable = false;
+        };
+        rust.enable = false;
+        typescript.enable = false;
+        null-ls.enable = true;
+        trouble.enable = true;
+        rename.enable = true;
+      };
+      statusline = {
+        enable = true;
+        theme = "auto";
+      };
+      surround.enable = true;
+      telescope = {
+        enable = true;
+        file_browser.enable = false;
+      };
+      theme = {
+        enable = true;
+        name = "rose-pine";
+        flavour = {
+          light = "dawn";
+          dark = "moon";
+        };
+      };
+      treesitter = {
+        enable = true;
+        autotag.enable = true;
+        context.enable = false;
+        rainbow.enable = true;
+        grammars = [
+          "css"
+          "dockerfile"
+          "eex"
+          "elixir"
+          "graphql"
+          "heex"
+          "html"
+          "json"
+          "markdown"
+          "nix"
+          "rust"
+          "scss"
+          "toml"
+          "yaml"
+        ];
+      };
+      ui = {
+        enable = true;
+        word_highlight.enable = true;
+        semantic_highlightment.enable = true;
+        matchup.enable = false;
+        which_key.enable = true;
+        legendary.enable = false;
+        noice.enable = false;
+        tabout.enable = false;
+      };
+      visuals = {
+        icons.enable = true;
+        cursorWordline.enable = true;
+        indentBlankline = {
+          enable = true;
+        };
+      };
+    };
+  };
+in
+  mkNeovim {inherit config;}
