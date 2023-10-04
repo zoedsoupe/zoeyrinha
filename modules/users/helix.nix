09f6ebb85e7331f572ec0b92c4bc210784e6b7e2
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2023-10-04 11:21:45 -0300

    feat: add xplr

--- modules/users/helix.nix
+++ modules/users/helix.nix
@@ -4,14 +4,20 @@
   custom-config,
   ...
 }: let
-  inherit (lib) mkEnableOption mkOption mkIf types;
+  inherit (lib) mkEnableOption mkIf;
   cfg = custom-config.helix;
+  erl = pkgs.beam.interpreters.erlang_25.overrideAttrs (old: {
+    configureFlags = ["--disable-jit"] ++ old.configureFlags;
+  });
+  beam = pkgs.beam.packagesWith erl;
+  vscode-lsp = pkgs.nodePackages.vscode-langservers-extracted;
 in {
   options.helix = {
     enable = mkEnableOption "Enbales Helix Editor";
   };
 
   config = mkIf cfg.enable {
+    home.packages = [beam.elixir-ls pkgs.clojure-lsp pkgs.nil vscode-lsp];
     programs.helix = {
       inherit (cfg) enable;
       settings = {
@@ -22,32 +28,6 @@ in {
         };
         keys.normal = {
           esc = ["collapse_selection" "keep_primary_selection"];
-        };
-      };
-      languages = {
-        language-server = let
-          erl = pkgs.beam.interpreters.erlang_25.overrideAttrs (old: {
-            configureFlags = ["--disable-jit"] ++ old.configureFlags;
-          });
-          beam = pkgs.beam.packagesWith erl;
-          vscode-lsp = pkgs.nodePackages.vscode-langservers-extracted;
-        in {
-          elixir-ls.command = "${beam.elixir-ls}/bin/elixir-ls";
-          nil.command = "${pkgs.nil}/bin/nil";
-          clojure-lsp.command = "${pkgs.clojure-lsp}/bin/clojure-lsp";
-
-          vscode-html-language-server = {
-            command = "${vscode-lsp}/bin/vscode-html-language-server";
-            args = ["--stdio"];
-          };
-          vscode-css-language-server = {
-            command = "${vscode-lsp}/bin/vscode-css-language-server";
-            args = ["--stdio"];
-          };
-          vscode-json-language-server = {
-            command = "${vscode-lsp}/bin/vscode-json-language-server";
-            args = ["--stdio"];
-          };
         };
       };
     };
