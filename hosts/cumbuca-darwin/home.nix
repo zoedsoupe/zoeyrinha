935cb996f5ab4faf1278f9c23430de4bfb6b90f3
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2023-10-30 17:38:28 -0300

    feat: cumbuca config for macos

---
+++ hosts/cumbuca-darwin/home.nix
@@ -0,0 +1,31 @@
+{pkgs, ...}: let
+  elixir_1_15 = pkgs.beam.packages.erlang_26.elixir_1_15;
+in {
+  home.stateVersion = "22.11";
+  home.homeDirectory = "/Users/zoeycumbuca";
+
+  programs = {
+    home-manager.enable = true;
+    command-not-found.enable = false;
+  };
+
+  home.packages = with pkgs; [
+    gnupg
+    elixir_1_15
+    gitAndTools.gh
+    ngrok
+
+    # tools
+    bat
+    # flyctl
+    eza # exa fork
+    fd
+    silver-searcher
+
+    # others
+    any-nix-shell
+
+    # audio & video
+    ffmpeg
+  ];
+}
