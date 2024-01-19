49758b9f604a62d6d2a39ebbffe72839904d9905
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2024-01-19 10:03:37 -0300

    upgrade

--- hosts/cumbuca/custom.nix
+++ hosts/cumbuca/custom.nix
@@ -24,6 +24,7 @@
       go.enable = false;
       zig.enable = false;
       nix.enable = true;
+      nim.enable = false;
       typescript.enable = true;
       elixir = let
         inherit (pkgs.beam) packagesWith;
